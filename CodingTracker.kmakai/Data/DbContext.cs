868e026fdea5929ca3d8da83b1a7ee289765fefb
kmakai <makaikwag@gmail.com>
2023-06-20 18:23:36 -0400

    added model fixed file extension will work on db

---
+++ CodingTracker.kmakai/Data/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+namespace CodingTracker.kmakai.Data;
+
+
+public class DbContext
+{
+    static string? ConnectionString = ConfigurationManager.AppSettings.Get("connectionString");
+
+
+}
